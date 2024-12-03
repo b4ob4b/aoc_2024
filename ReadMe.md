8d4edb4f6510374a5b29289ebf5db4522b186b0b
Julian Baumgaertner <julian.baumgaertner@bmw.de>
2024-12-03 06:51:59 +0100

    Day3: Mull It Over

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,4 @@ Here you find my solutions written in Kotlin:
 
 * [Day 1: Historian Hysteria](src/main/kotlin/Day01.kt)
 * [Day 2: Red-Nosed Reports](src/main/kotlin/Day02.kt)
+* [Day 3: Mull It Over](src/main/kotlin/Day03.kt)
--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,21 +1,31 @@
 import utils.*
 
 fun main() {
-    Day03(IO.TYPE.SAMPLE).test()
+    Day03(IO.TYPE.SAMPLE).test(161)
+    Day03(IO.TYPE.SAMPLE2).test(part2 = 48)
     Day03().solve()
 }
 
-class Day03(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
+class Day03(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Mull It Over", inputType = inputType) {
 
-    private val data = input.splitLines()
-    
-    override fun part1(): Any? {
-        data
-            .print()
-        return "not yet implement"
+    private val memory = input.replace("\n", "")
+
+    override fun part1(): Int = memory.parseMultiplications().sum()
+
+    override fun part2(): Int {
+        val pattern = "don't\\(\\).*?do\\(\\)".toRegex()
+        return memory
+            .replace(pattern, "")
+            .parseMultiplications()
+            .sum()
     }
 
-    override fun part2(): Any? {
-        return "not yet implement"
+    private fun String.parseMultiplications(): List<Int> {
+        val pattern = "mul\\(\\d+,\\d+\\)".toRegex()
+        val matches = pattern.findAll(this)
+        return matches.toList().map { match ->
+            match.value.extractInts()
+                .product()
+        }
     }
 }           
