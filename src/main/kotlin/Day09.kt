5f7305f8ff0602a91937875c0bccf662f6b43994
Julian Baumgaertner <julian.baumgaertner@bmw.de>
2024-12-09 14:28:08 +0100

    Day9: Disk Fragmenter

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -26,30 +26,29 @@ class Day09(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Disk Fragmenter", inputTy
     override fun part1(): Long {
         val memory = memory.toMutableList()
         while (memory.hasFreeSpace()) {
-            val emtpyBlock = memory.withIndex().first { (i, b) -> b.type == Type.FREE }
-            val blockToMove = memory.withIndex().last { (i, b) -> b.type == Type.USED }
+            val emptyBlock = memory.withIndex().first { it.value.type == Type.FREE }
+            val blockToMove = memory.withIndex().last { it.value.type == Type.USED }
 
-            if (emtpyBlock.value.length == blockToMove.value.length) {
-                memory.removeAt(blockToMove.index)
-                memory[emtpyBlock.index] = blockToMove.value
-            } else if (emtpyBlock.value.length > blockToMove.value.length) {
-                memory.removeAt(blockToMove.index)
-                memory.removeAt(emtpyBlock.index)
-                memory.addAll(
-                    emtpyBlock.index,
-                    listOf(
-                        blockToMove.value,
-                        Block(null, Type.FREE, emtpyBlock.value.length - blockToMove.value.length)
+            when {
+                emptyBlock.value.length == blockToMove.value.length -> {
+                    memory[emptyBlock.index] = blockToMove.value
+                    memory.removeAt(blockToMove.index)
+                }
+
+                emptyBlock.value.length > blockToMove.value.length -> {
+                    memory.removeAt(blockToMove.index)
+                    memory[emptyBlock.index] = blockToMove.value
+                    memory.add(
+                        emptyBlock.index + 1,
+                        Block(null, Type.FREE, emptyBlock.value.length - blockToMove.value.length)
                     )
-                )
-            } else if (emtpyBlock.value.length < blockToMove.value.length) {
-                memory[blockToMove.index] = Block(
-                    blockToMove.value.value, Type.USED, blockToMove.value.length - emtpyBlock
-                        .value.length
-                )
-                memory[emtpyBlock.index] = Block(
-                    blockToMove.value.value, Type.USED, emtpyBlock.value.length
-                )
+                }
+
+                else -> {
+                    memory[blockToMove.index] =
+                        Block(blockToMove.value.value, Type.USED, blockToMove.value.length - emptyBlock.value.length)
+                    memory[emptyBlock.index] = Block(blockToMove.value.value, Type.USED, emptyBlock.value.length)
+                }
             }
 
             if (memory.last().type == Type.FREE) {
@@ -62,47 +61,52 @@ class Day09(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Disk Fragmenter", inputTy
     override fun part2(): Long {
         val memory = memory.toMutableList()
 
-        var c = memory.count { it.type == Type.USED } - 1
+        var indexToCheck = memory.count { it.type == Type.USED } - 1
 
-        while (c > 0) {
+        while (indexToCheck > 0) {
             val blockToMove = memory.withIndex()
                 .filter { (i, block) ->
                     block.type == Type.USED
                 }.withIndex()
                 .last { (filteredIndex, globalIndexedBlock) ->
                     val (_, block) = globalIndexedBlock
-                    block.type == Type.USED && filteredIndex == c
+                    block.type == Type.USED && filteredIndex == indexToCheck
                 }.value
 
-            if (memory.take(blockToMove.index).filter { it.type == Type.FREE }.none {
-                    it.length >= blockToMove.value
-                        .length
-                }) {
-                c--
+            if (blockToMove.isNotMovable(memory)) {
+                indexToCheck--
                 continue
             }
 
-            val emtpyBlock = memory.withIndex()
+            val emptyBlock = memory.withIndex()
                 .first { (i, block) ->
                     block.type == Type.FREE && block.length >= blockToMove.value.length
                 }
 
-            if (emtpyBlock.value.length == blockToMove.value.length) {
-                memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
-                memory[emtpyBlock.index] = blockToMove.value
-            } else if (emtpyBlock.value.length > blockToMove.value.length) {
-                memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
-                memory.removeAt(emtpyBlock.index)
-                memory.addAll(
-                    emtpyBlock.index,
-                    listOf(
-                        blockToMove.value,
-                        Block(null, Type.FREE, emtpyBlock.value.length - blockToMove.value.length)
+            when {
+                emptyBlock.value.length == blockToMove.value.length -> {
+                    memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
+                    memory[emptyBlock.index] = blockToMove.value
+                }
+
+                emptyBlock.value.length > blockToMove.value.length -> {
+                    memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
+                    memory.removeAt(emptyBlock.index)
+                    memory.addAll(
+                        emptyBlock.index,
+                        listOf(
+                            blockToMove.value,
+                            Block(null, Type.FREE, emptyBlock.value.length - blockToMove.value.length)
+                        )
                     )
-                )
+                }
             }
         }
         return memory.calculateCheckSum()
+    }
+
+    private fun IndexedValue<Block>.isNotMovable(memory: List<Block>): Boolean {
+        return memory.take(index).none { it.type == Type.FREE && it.length >= value.length }
     }
 
     private fun List<Block>.hasFreeSpace(): Boolean = any { it.type == Type.FREE }
@@ -113,16 +117,4 @@ class Day09(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Disk Fragmenter", inputTy
         }
         Pair(block.length + index, overAllCheckSum + checkSum)
     }.second
-
-    private fun List<Block>.print1() {
-        forEach { block ->
-            when (block.type) {
-                Type.USED -> repeat(block.length) { print("(${block.value})") }
-//                Type.USED -> repeat(block.length) { print(block.value) }
-                Type.FREE -> repeat(block.length) { print(".") }
-            }
-        }.also {
-            println()
-        }
-    }
-}           
+}
