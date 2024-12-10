52e7572758f73a56854b437558f6908f6b67e84a
Julian Baumgaertner <julian.baumgaertner@bmw.de>
2024-12-10 20:36:23 +0100

    Day 10: refactored

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -8,9 +8,9 @@ fun main() {
 
 class Day10(dataType: () -> DataType) : Day("Hoof It", dataType) {
 
-    private val field = input.toGrid { it.toInt() }.toField()
+    private val hill = input.toGrid { it.toInt() }.toField()
     private val paths
-        get() = field
+        get() = hill
             .search(0)
             .flatMap {
                 Step(it, it, 0).walk()
@@ -20,30 +20,21 @@ class Day10(dataType: () -> DataType) : Day("Hoof It", dataType) {
 
     override fun part2() = paths.count()
 
-    data class Step(
+    private fun Sequence<Step>.countUnique() = map { it.position to it.origin }.toSet().size
+
+    private data class Step(
         val origin: Position,
         val position: Position,
         val height: Int,
     )
 
-    private fun Sequence<Step>.countUnique() =
-        map { it.position to it.origin }
-            .toSet()
-            .size
-
-    private fun Step.walk(): List<Step> {
-        if (height == 9) return listOf(this)
-
-        val nextPositions = Direction4.entries.map { direction ->
-            position.doMovement(direction)
-        }.filter {
-            it in field && field[it] == height + 1
-        }
-
-        if (nextPositions.isEmpty()) return emptyList()
-
-        return nextPositions.flatMap { nextPosition ->
-            Step(origin, nextPosition, height + 1).walk()
-        }
+    private fun Step.walk(): Sequence<Step> = sequence {
+        if (height == 9) yield(this@walk)
+        Direction4.entries
+            .map { direction -> position.doMovement(direction) }
+            .filter { it in hill && hill[it] == height + 1 }
+            .forEach { nextPosition ->
+                yieldAll(Step(origin, nextPosition, height + 1).walk())
+            }
     }
 }
